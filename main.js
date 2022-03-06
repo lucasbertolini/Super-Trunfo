d57db84373068522a77b158f00c15294196dd125
Lucas Bertolini <42697227+lucasbertolini@users.noreply.github.com>
2022-03-06 16:34:19 -0300

    Added images

--- main.js
+++ main.js
@@ -2,30 +2,33 @@
 const cartas = {
     0:{
       atributos:{
-        velocidade:1300,
-        empuxo:3500,
-        envergadura:7.70,
+        peso:946,
+        cilindrada:999,
+        velocidadeMax:163,
+        nome:"Gol"
         
     },
-      img:"",
+      img:"<img  class='imagenCartaJogador' src='./img/vw-gol.png' rel='VW Gol'>",
     },
     1:{
       atributos:{
-        velocidade:1000,
-        empuxo:3000,
-        envergadura:6.70,
+        peso:1500,
+        cilindrada:1400,
+        velocidadeMax:238,
+        nome:'Golf'
         
     },
-      img:"",
+      img:"<img class='imagenCartaJogador' src='./img/vw-golf.png' rel='VW Golf'>",
     },
     2:{
       atributos:{
-        velocidade:1500,
-        empuxo:4500,
-        envergadura:4.70,
+        peso:1721,
+        cilindrada:1968,
+        velocidadeMax:218,
+        nome:'Passat'
         
     },
-      img:"",
+      img:"<img class='imagenCartaJogador' src='./img/vw-passat.png' rel='VW Passat'>",
     }
 }
 const cartaMaquinaTexto = document.getElementById("cartaMaquina");
@@ -33,6 +36,7 @@ const cartaJogadorTexto = document.getElementById("cartaJogador");
 const resultado = document.getElementById('resultado');
 const jogadorGanhasTexto = document.getElementById('jogadorVitorias');
 const maquinaGanhasTexto = document.getElementById('maquinaVitorias');
+const jogarBtn = document.querySelector('#botao');
 let maquinaGanhas = 0;
 let jogadorGanhas = 0;
 let cartaMaquina 
@@ -49,80 +53,93 @@ function gerarNumero(){
 }
 function jogar(){
     
-    const escolhaJogador = document.querySelectorAll('input[name = "atributoJogador"]'); 
-    for(const botaoSelecionado of escolhaJogador){
-          if(botaoSelecionado.checked){
-            radioSelecionado = botaoSelecionado.value;
-            switch (radioSelecionado) {
-              case 'velocidade':
-                if(cartas[cartaJogador].atributos.velocidade == cartas[cartaMaquina].atributos.velocidade){
-                  resultado.innerHTML = 'Empate';
-                }else if(cartas[cartaJogador].atributos.velocidade > cartas[cartaMaquina].atributos.velocidade){
-                  resultado.innerHTML = 'Jogador venceu';
-                  jogadorGanhas++;
-                }else if (cartas[cartaJogador].atributos.velocidade < cartas[cartaMaquina].atributos.velocidade){
-                  resultado.innerHTML = 'Maquina venceu ';
-                  maquinaGanhas++;
-                }
-                break;
-              case 'empuxo':
-                if(cartas[cartaJogador].atributos.empuxo == cartas[cartaMaquina].atributos.empuxo){
-                  resultado.innerHTML = 'Empate';
-                }else if(cartas[cartaJogador].atributos.empuxo > cartas[cartaMaquina].atributos.empuxo){
-                  resultado.innerHTML = 'Jogador venceu';
-                  jogadorGanhas++;
-                }else if(cartas[cartaJogador].atributos.empuxo < cartas[cartaMaquina].atributos.empuxo){
-                  resultado.innerHTML = 'Maquina venceu';
-                  maquinaGanhas++;
-                }
-                break;
-              case 'envergadura':
-                if(cartas[cartaJogador].atributos.envergadura == cartas[cartaMaquina].atributos.envergadura){
-                  resultado.innerHTML = 'Empate';
-                }else if(cartas[cartaJogador].atributos.envergadura > cartas[cartaMaquina].atributos.envergadura){
-                  resultado.innerHTML = 'Jogador venceu';
-                  jogadorGanhas++;
-                }else if(cartas[cartaJogador].atributos.envergadura < cartas[cartaMaquina].atributos.envergadura){
-                  resultado.innerHTML = 'Maquina venceu';
-                  maquinaGanhas++;
-                }
-                break;
-
-            }
-
-            cartaMaquinaTexto.innerHTML = 
-              `Velocidade ${cartas[cartaMaquina].atributos.velocidade} <Br>
-              Empuxo ${cartas[cartaMaquina].atributos.empuxo} <br>
-              Envergadura ${cartas[cartaMaquina].atributos.envergadura}`
-            };
-            jogadorGanhasTexto.innerHTML = ` ${jogadorGanhas}`;
-            maquinaGanhasTexto.innerHTML = ` ${maquinaGanhas}`;
-
-
+  const escolhaJogador = document.querySelectorAll('input[name = "atributoJogador"]'); 
+  for(const botaoSelecionado of escolhaJogador){
+    if(botaoSelecionado.checked){
+      jogarBtn.disabled = true;
+      radioSelecionado = botaoSelecionado.value;
+      switch (radioSelecionado) {
+        
+        case 'peso':
+          if(cartas[cartaJogador].atributos.peso == cartas[cartaMaquina].atributos.peso){
+            resultado.innerHTML = 'Empate';
+          }else if(cartas[cartaJogador].atributos.peso < cartas[cartaMaquina].atributos.peso){
+            resultado.innerHTML = 'Jogador venceu';
+            jogadorGanhas++;
+          }else if (cartas[cartaJogador].atributos.peso > cartas[cartaMaquina].atributos.peso){
+            resultado.innerHTML = 'Maquina venceu ';
+            maquinaGanhas++;
           }
+          break;
+          case 'cilindrada':
+            if(cartas[cartaJogador].atributos.cilindrada == cartas[cartaMaquina].atributos.cilindrada){
+              resultado.innerHTML = 'Empate';
+            }else if(cartas[cartaJogador].atributos.cilindrada > cartas[cartaMaquina].atributos.cilindrada){
+              resultado.innerHTML = 'Jogador venceu';
+              jogadorGanhas++;
+            }else if(cartas[cartaJogador].atributos.cilindrada < cartas[cartaMaquina].atributos.cilindrada){
+              resultado.innerHTML = 'Maquina venceu';
+              maquinaGanhas++;
+            }
+            break;
+          case 'velocidadeMax':
+            if(cartas[cartaJogador].atributos.velocidadeMax == cartas[cartaMaquina].atributos.velocidadeMax){
+              resultado.innerHTML = 'Empate';
+            }else if(cartas[cartaJogador].atributos.velocidadeMax > cartas[cartaMaquina].atributos.velocidadeMax){
+              resultado.innerHTML = 'Jogador venceu';
+              jogadorGanhas++;
+            }else if(cartas[cartaJogador].atributos.velocidadeMax < cartas[cartaMaquina].atributos.velocidadeMax){
+              resultado.innerHTML = 'Maquina venceu';
+              maquinaGanhas++;
+            }
+            break;
+
+      }
+
+      cartaMaquinaTexto.innerHTML = 
+        `<div class='cartaMaquinaImg'>
+          ${cartas[cartaMaquina].img}
+        </div>
+        <h3>${cartas[cartaMaquina].atributos.nome}</h3>
+        <span>Peso: ${cartas[cartaMaquina].atributos.peso}</span> <Br>
+        <span>Cilindrada: ${cartas[cartaMaquina].atributos.cilindrada}</span> <br>
+        <span>Velocidade maxima: ${cartas[cartaMaquina].atributos.velocidadeMax} </span>`
+    };
+    jogadorGanhasTexto.innerHTML = ` ${jogadorGanhas}`;
+    maquinaGanhasTexto.innerHTML = ` ${maquinaGanhas}`;
+            
+            
+  }
+
+ 
+
 }
 function comecar(){
     cartaJogador = gerarNumero();
     cartaMaquina = gerarNumero();
+    while(cartaJogador == cartaMaquina) cartaMaquina = gerarNumero();
     escreverAtributos();
     resultado.innerHTML = " ";
     cartaMaquinaTexto.innerHTML = " ";
     jogadorGanhasTexto.innerHTML = ` ${jogadorGanhas}`;
     maquinaGanhasTexto.innerHTML = ` ${maquinaGanhas}`;
+    jogarBtn.disabled = false;
   
 }
 
 function escreverAtributos(){
     cartaJogadorTexto.innerHTML =  
-    `<input type="radio" id="velocidade" name="atributoJogador" value="velocidade" > 
-    <label for ="velocidade">Velocidade</label> ${cartas[cartaJogador].atributos.velocidade} <br>
-    <input type="radio" id="empuxo" name="atributoJogador" value="empuxo">
-    <label for="empuxo">Empuxo</label> ${cartas[cartaJogador].atributos.empuxo} <br>
-    <input type="radio" id="envergadura" name="atributoJogador" value="envergadura" >
-    <label for="envergadura">Envergadura</label> ${cartas[cartaJogador].atributos.envergadura}
+    `<div class='cartaJogadorImg'>${cartas[cartaJogador].img}</div>
+    <h3>${cartas[cartaJogador].atributos.nome}</h3>
+    <input type="radio" id="peso" name="atributoJogador" value="peso" > 
+    <label for ="peso">Peso: ${cartas[cartaJogador].atributos.peso}</label>  <br>
+    <input type="radio" id="cilindrada" name="atributoJogador" value="cilindrada">
+    <label for="cilindrada">Cilindrada: ${cartas[cartaJogador].atributos.cilindrada}</label> <br>
+    <input type="radio" id="velocidadeMax" name="atributoJogador" value="velocidadeMax" >
+    <label for="velocidadeMax">Velocidade Maxima: ${cartas[cartaJogador].atributos.velocidadeMax}</label> 
 
   `;
-}
-  
 
+
+}
 
